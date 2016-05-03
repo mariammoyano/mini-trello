21ba5af87d0abd0bb76e0aa2a4e048cdff4ffdf6
Maria Mercedes Moyano <moyanomariamercedes@gmail.com>
2016-05-03 17:50:06 -0300

    can persist and cancel state edits

--- app/js/Directives/state-edit.directive.js
+++ app/js/Directives/state-edit.directive.js
@@ -4,10 +4,31 @@ function stateEdit() {
     	restrict: 'AE',
     	scope: {
     		form: '=',
-    		state: '='
+    		state: '=',
+    		action: '&'
+    	},
+    	//TODO: move link function
+    	link: function(scope, elem, attrs){
+    		scope.editState = angular.copy(scope.state);
+    		scope.toggleEdit = function(){
+    			scope.editing = !scope.editing;
+    		};
+    		scope.saveState = function(){
+    			angular.copy(scope.editState, scope.state);
+    			scope.action();
+    			this.toggleEdit();
+    		};
+    		scope.reset = function(){
+    			angular.copy(scope.state, scope.editState);
+    		}
+    		scope.cancelEdit = function(){
+    			scope.reset();
+    			scope.editing = false;
+    		}
     	},
     	templateUrl: "/app/js/Directives/state-edit.html"
     }
+
 }
 
 export default stateEdit;
